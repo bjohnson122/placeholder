60cceafa5f0784e283364be51bbd98e96f10c784
Breana Johnson <brecjohnson@gmail.com>
2023-01-25 03:18:58 -0500

    nav and footer working on web

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -4,9 +4,10 @@ import Meta from "./Meta";
 import Navbar from "./Navbar";
 // import styles from "./Layout.module.css";
 
+
 function Layout({ children }) {
   return (
-    <div>
+    <div className='font-raleway font-normal'>
       <Meta title="Designer and Developer" />
       <Navbar />
       <main>{children}</main>
--- src/pages/hero/Hero.js
+++ src/pages/hero/Hero.js
@@ -5,7 +5,7 @@ import styles from './Hero.module.css'
 export default function Hero() {
   return (
     <div>
-        <h1 className='font-zidan text-center '>Breana Johnson</h1>
+        <h1 className='font-zidan text-center text-7xl font-normal'>Breana Johnson</h1>
        
     </div>
   )
