d5aff05e916ca770a8ac62841f86653630650db9
Breana Johnson <brecjohnson@gmail.com>
2023-01-27 02:38:40 -0500

    fixed bug

--- src/pages/hero/Hero.js
+++ src/pages/hero/Hero.js
@@ -6,7 +6,7 @@ import styles from './Hero.module.css'
 export default function Hero() {
   return (
     <div>
-        <h1 className='font-zidan text-center text-7xl font-normal pt-[1rem]'>Breana Johnson</h1>
+        <h1 className='font-zidan text-center text-7xl mt-[2rem] md:mt-[1rem]'>Breana Johnson</h1>
        
     </div>
   )
