9411f0a01eac8fff87bf24c6f4ec350cd4be28c9
Breana Johnson <brecjohnson@gmail.com>
2023-01-28 01:56:45 -0500

    updated spacing for mobile

--- src/components/loader/Loader.js
+++ src/components/loader/Loader.js
@@ -19,11 +19,13 @@ export default function Loader() {
             autoStart: true,
             loop: true,
             delay: 100,
+            changeDeleteSpeed:70,
+            pauseFor:1000
           }}
         />
       </div>
 
-      <div className="font-raleway text-center w-full sm:text-green-500 md:text-pink-500">
+      <div className="font-raleway text-center w-full sm:text-green-500 md:text-pink-500 fixed bottom-[19rem] font-semibold">
         Software Engineer & Designer
       </div>
     </div>
