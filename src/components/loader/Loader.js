4da7076558335b4d39f4f13bc52bf93e6b64ad83
Breana Johnson <brecjohnson@gmail.com>
2023-01-28 01:38:29 -0500

    centered typing animation text

---
+++ src/components/loader/Loader.js
@@ -0,0 +1,31 @@
+import React from "react";
+import Typewriter from "typewriter-effect";
+
+// new Typewriter('#typewriter', {
+//   strings: ['Hello', 'World'],
+//   autoStart: true,
+// });
+
+export default function Loader() {
+  return (
+    <div className=" ">
+      <div
+        id="loader"
+        className="font-zidan text-8xl w-full text-center pt-[calc(100vh/3.5)] fixed tracking-[.05em] p-8"
+      >
+        <Typewriter
+          options={{
+            strings: ["Breana  Johnson"],
+            autoStart: true,
+            loop: true,
+            delay: 100,
+          }}
+        />
+      </div>
+
+      <div className="fixed  md:relative font-raleway text-2xl md:align-center md:text-center md:mt-4 md:pt-4">
+        Software Engineer & Designer
+      </div>
+    </div>
+  );
+}
---
+++ src/pages/load.js
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from "react";
+import Loader from "../components/loader/Loader";
+function Load() {
+  useEffect(() => {});
+  return (
+    <div className="z-50 ">
+      <Loader  />
+    </div>
+  );
+}
+
+export default Load;
